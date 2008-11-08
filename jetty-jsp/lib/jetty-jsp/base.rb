03b4226a3a3275835ec5e9187e3c8040af83d0be
(no author) <(no author)@ba0075bf-330a-0410-8768-ef3ec23a1e87>
2008-11-08 01:15:53 +0000

    relocated
    git-svn-id: svn://localhost/subversion.repo/src/gems/trunk@489 ba0075bf-330a-0410-8768-ef3ec23a1e87

---
+++ jetty-jsp/lib/jetty-jsp/base.rb
@@ -0,0 +1,25 @@
+#--
+# Copyright (C) 2008 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+module JettyJspBase
+  JETTY_VERSION = '6.1.11'
+  JSP_VERSION = '2.1'
+  GEM_VERSION = '1'
+
+  VERSION = [ JETTY_VERSION, JSP_VERSION, GEM_VERSION ].join( '.' ) 
+
+  JETTY_JSP_DIR = File.dirname( __FILE__ ) # :nodoc:
+end

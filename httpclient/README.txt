31a4049918f2cc0245470f65e30a6023eb5d6f4e
David Kellum <dek-oss@gravitext.com>
2009-02-16 14:13:21 -0800

    Initial import of (apache) httpclient 3.1 gem for possible addition.

---
+++ httpclient/README.txt
@@ -0,0 +1,61 @@
+= httpclient
+
+* http://rjack.rubyforge.org
+* http://rubyforge.org/projects/rjack
+
+== Description
+
+A gem packaging of the {Apache HttpClient}[http://hc.apache.org/] 
+for JRuby:
+
+* Provides commons-httpclient and commons-codec jars.
+
+== Synopsis
+
+TBD
+
+== Requirements
+
+Either of the following should be loaded before httpclient:
+
+* SLF4J (require 'sl4j/jcl-over-slf4j') (available as rjack
+  slf4j[http://rjack.rubyforge.org/slf4j] gem), or:
+
+* Apache Commons Logging jar (not available from rjack)
+
+== License
+
+=== httpclient ruby gem
+
+Copyright (C) 2009 David Kellum
+
+Licensed under the Apache License, Version 2.0 (the "License"); you
+may not use this file except in compliance with the License.  You
+may obtain a copy of the License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied.  See the License for the specific language governing
+permissions and limitations under the License.
+
+=== Apache Jakarta HTTPClient (Java)
+
+Copyright 1999-2007 The Apache Software Foundation
+
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version
+2.0 (the "License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied.  See the License for the specific language governing
+permissions and limitations under the License.
---
+++ httpclient/lib/httpclient.rb
@@ -0,0 +1,78 @@
+#--
+# Copyright (C) 2009 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+require 'httpclient/base'
+
+#FIXME: Naming conflict: http://dev.ctor.org/http-access2
+
+module HTTPClient
+
+  Dir.glob( File.join( HTTPCLIENT_DIR, '*.jar' ) ).each { |jar| require jar }
+
+  import 'org.apache.commons.httpclient.params.HttpConnectionManagerParams'
+  import 'org.apache.commons.httpclient.params.HttpClientParams'
+  import 'org.apache.commons.httpclient.params.HttpMethodParams'  
+  import 'org.apache.commons.httpclient.DefaultHttpMethodRetryHandler'
+  import 'org.apache.commons.httpclient.MultiThreadedHttpConnectionManager'
+  import 'org.apache.commons.httpclient.HttpClient'
+
+  # See: http://hc.apache.org/httpclient-3.x/preference-api.html
+  # 
+  # FIXME: Replace below with set_parameter( string, value ) examples.
+  # Example settings:
+  #  manager_params.max_total_connections = 200
+  #  manager_params.connection_timeout = 1500 #ms
+  #  manager_params.default_max_connections_per_host = 20
+  #  manager_params.stale_checking_enabled = false
+  #  client_params.connection_manager_timeout = 3000 #ms
+  #  client_params.so_timeout = 3000 #ms
+  #  client_params.set_parameter( HttpMethodParams::RETRY_HANDLER, 
+  #                               DefaultHttpMethodRetryHandler.new( 2, false ) )
+  #  client_params.cookie_policy = CookiePolicy::IGNORE_COOKIES
+  class ManagerFacade
+
+    # The HttpClient instance available after start
+    attr_reader :client
+    
+    # Manager paramters
+    attr_reader :manager_params
+
+    # Client paramters
+    attr_reader :client_params
+
+    def initialize
+      @manager_params = HttpConnectionManagerParams.new
+
+      @client_params = HttpClientParams.new
+      
+      @client = nil
+      @connection_manager = nil
+    end
+
+    def start
+      @connection_manager = MultiThreadedHttpConnectionManager.new()
+      @connection_manager.params = @manager_params
+
+      @client = HttpClient.new( @client_params, @connection_manager );
+    end
+
+    def shutdown
+      @connection_manager.shutdown if @connection_manager
+      @client = nil
+      @connection_manager = nil
+    end
+  end
+end

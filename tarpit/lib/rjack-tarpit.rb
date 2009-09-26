187c3c18704bf6ba0917fbb0ab15c4f9950c3b84
David Kellum <dek-oss@gravitext.com>
2009-09-26 14:31:43 -0700

    rjack-tarpit docs

--- tarpit/lib/rjack-tarpit.rb
+++ tarpit/lib/rjack-tarpit.rb
@@ -1,3 +1,19 @@
+#--
+# Copyright (C) 2009 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
 require 'hoe'
 
 # Silly Hoe sets up DOT with rdoc unless this is set.
@@ -5,49 +21,86 @@ ENV['NODOT'] = "no thanks"
 
 module RJack
 
+  # Provides glue for Rake, Hoe, and Maven by generating tasks.
   module TarPit
+    # Module version
     VERSION = '1.0.0'
 
-    def self.new( p, *flags )
+    # Construct new task generator by gem name, version, and flags. A descendant
+    # of BaseStrategy is returned.
+    # ==== flags
+    # :jars_from_assembly:: jars will be found in assembly rather then
+    #                       set in Rakefile.
+    # :no_assembly:: One jar created from source, no assembly setup in maven.
+    def self.new( name, version, *flags )
       if flags.include?( :jars_from_assembly )
-        JarsFromAssembly.new( p, flags )
+        JarsFromAssembly.new( name, version, flags )
       else
-        BaseStrategy.new( p, flags )
+        BaseStrategy.new( name, version, flags )
       end
     end
 
+    # Base strategy implementation where jars are known by the
+    # Rakefile (not :jars_from_assembly)
     class BaseStrategy
 
+      # Name of gem as constructed.
       attr_reader :name
+      # Version of gem as constructed.
+      attr_reader :version
 
-      attr_accessor :version
-      attr_accessor :assembly_name
-      attr_accessor :assembly_version
+      # The set of jar file names (without path) to include. May be
+      # lazily computed by other strategies.
       attr_accessor :jars
+
+      # Destination path for any jars [Default: lib/name]
       attr_accessor :jar_dest
+
+      # Any additional generated files to be included [Default: nil]
       attr_accessor :generated_files
 
-      def initialize( name, flags )
+      # The name of the assembly [Default: name]
+      attr_writer :assembly_name
+
+      # The version of the assembly, which might be static
+      # (i.e. "1.0") if the pom is not shared (dependency jars only)
+      # [Default: version]
+      attr_writer :assembly_version
+
+      # See TarPit.new
+      def initialize( name, version, flags )
         @name = name
+        @version = version
         @flags = flags
         @jars = nil
         @jar_dest = File.join( 'lib', @name )
         @hoe_specifier = :unset
       end
 
-      # Specify gem project details, yielding hoe instance to block after
-      # setting various defaults.
+      # Return a default jar name built from name and version
+      def default_jar
+        "#{name}-#{version}.jar"
+      end
+
+      # Specify gem project details, yielding Hoe instance to block
+      # after setting various defaults. Thus all Hoe.spec setters are
+      # valid in this block. The actual Hoe construction and execution
+      # of block is deferred to define_tasks.
       def specify( &block )
         @hoe_specifier = block
       end
 
+      # Define all tasks based on provided details. In this strategy,
+      # the Manifest.txt task will be invoked prior to calling
+      # Hoe.spec, thus any additional Manifest.txt dependencies
+      # should be specified prior to this call.
       def define_tasks
         define_maven_package_task if jars
 
-        if jars || @generated_files
+        if jars || generated_files
           mtask = define_manifest_task
 
-          # The manifiest needs to run before hoe_specify
+          # The manifest needs to run before hoe_specify
           mtask.invoke
         end
 
@@ -58,8 +111,8 @@ module RJack
         define_git_tag
       end
 
-      # Define task for dynamicly generating Manifest.txt, by
-      # appending array returned by the given block to specifed files, or
+      # Define task for dynamically generating Manifest.txt, by
+      # appending array returned by the given block to specified files, or
       # contents of Manifest.static.
       def define_manifest_task
 
@@ -67,7 +120,7 @@ module RJack
           file 'Manifest.txt' => [ 'Manifest.static' ]
         end
 
-        gf = clean_list( @generated_files ).sort
+        gf = clean_list( generated_files ).sort
         [ :gem, :test ].each { |t| task t => gf }
 
         unless gf.empty?
@@ -89,38 +142,58 @@ module RJack
         mtask
       end
 
-      # A file used to record the time of last 'mvn package' run.
+      # File touched to record the time of last successful 'mvn
+      # package' run.
       MVN_STATE_FILE = 'target/.tarpit'
 
+      # Define a file task tracking calls to "mvn package"
       def define_maven_package_task
         file MVN_STATE_FILE => maven_dependencies do
-          sh( 'mvn package' ) && touch( MVN_STATE_FILE )
+          sh( 'mvn package' ) do |ok,res|
+            if ok
+              touch( MVN_STATE_FILE )
+            else
+              raise "TARPIT: 'mvn package' failed."
+            end
+          end
         end
       end
 
+      # Define file tasks for all jar symlinks and other misc. maven
+      # associated tasks like :mvn_clean.
       def define_post_maven_tasks
-        ap = jars_from_path
+        jfrom = jar_from
         jars.each do |jar|
-          from = File.join( ap, jar )
-          dest = File.join( @jar_dest, jar )
+          from = File.join( jfrom, jar )
+          dest = File.join( jar_dest, jar )
           file from => [ MVN_STATE_FILE ]
           file dest => [ from ] do
             ln_sf( File.join( '..', '..', from ), dest )
           end
           puts "TARPIT: :test, :gem => #{dest} => #{from}"
-          [ :gem, :test ].each { |t| task t => [ dest ] } #FIXME
+          [ :gem, :test ].each { |t| task t => [ dest ] }
         end
 
         task :mvn_clean do
-          rm_f jar_dest_files #FIXME: Use pattern instead?
+          remove_dest_jars
           sh 'mvn clean'
         end
         task :clean => :mvn_clean
       end
 
+      # Dependencies on "mvn package" including pom.xml, any assembly.xml,
+      # all files under the "src" directory.
+      def maven_dependencies
+        deps  = [ 'pom.xml' ]
+        deps << 'assembly.xml' if File.exist?( 'assembly.xml' )
+        deps += FileList[ "src/**/*" ].exclude { |f| ! File.file?( f ) }
+        deps
+      end
+
+      # Define git based :tag task
       def define_git_tag
         task :tag do
-          tag = [ @name, @version ].join( '-' )
+          tag = [ name, version ].join( '-' )
           dname = File.dirname( __FILE__ )
           dname = '.' if Dir.getwd == dname
           sh( "git status --only #{dname}" ) do |ok,res|
@@ -132,6 +205,7 @@ module RJack
         end
       end
 
+      # Setup Hoe via Hoe.spec
       def hoe_specify
         unless @hoe_specifier == :unset
           tp = self
@@ -148,31 +222,34 @@ module RJack
         end
       end
 
+      # Generate Manifest.txt
       def generate_manifest
-        # FIXME: Clean old lib/*.jar links?
+        remove_dest_jars
+
         m = []
         if File.exist?( 'Manifest.static' )
-          m += read_static_files( 'Manifest.static' )
+          m += read_file_list( 'Manifest.static' )
         end
-        m += clean_list( @generated_files ).sort
-        m += jar_dest_files
+        m += clean_list( generated_files ).sort
+        m += dest_jars
 
         puts "TARPIT: Updating Manifest.txt"
         open( 'Manifest.txt', 'w' ) { |out| out.puts m }
       end
 
-      def maven_dependencies
-        deps  = [ 'pom.xml' ]
-        deps << 'assembly.xml' if File.exist?( 'assembly.xml' )
-        deps += FileList[ "src/**/*" ].exclude { |f| ! File.file?( f ) }
-        deps
+      # Remove jars in jar_dest by wildcard expression
+      def remove_dest_jars
+        jars = FileList[ File.join( jar_dest, "*.jar" ) ].sort
+        rm_f jars unless jars.empty?
       end
 
-      def jar_dest_files
-        clean_list( jars ).sort.map { |j| File.join( @jar_dest, j ) }
+      # The list of jars in jar_dest path, used during manifest generation
+      def dest_jars
+        clean_list( jars ).sort.map { |j| File.join( jar_dest, j ) }
       end
 
-      def jars_from_path
+      # The target/assembly path from which jars are linked
+      def jar_from
         dirs = [ 'target' ]
 
         unless @flags.include?( :no_assembly )
@@ -184,10 +261,12 @@ module RJack
         File.join( dirs )
       end
 
-      def read_static_files( sfile )
+      # Read a list of files and return a cleaned list.
+      def read_file_list( sfile )
         clean_list( open( sfile ) { |f| f.readlines } )
       end
 
+      # Cleanup a list of files
       def clean_list( l )
         l = l.to_a.compact
         l.map! { |f| f.strip }
@@ -198,17 +277,20 @@ module RJack
 
     end
 
+    # Strategy in which the jars are inferred only by inspecting the
+    # the completed maven assembly. The :manifest task is thus
+    # dependent on 'mvn package'. Manifest changes need to be manually
+    # incorporated by explicitly running "jrake manifest"
     class JarsFromAssembly < BaseStrategy
 
+      # Define all tasks based on provided details. Don't invoke
+      # Manifest.txt before Hoe, just use whats already in place.
       def define_tasks
         define_maven_package_task
 
         mtask = define_manifest_task
 
         task :manifest => [ MVN_STATE_FILE ]
-
-        # The manifiest needs to run before hoe_specify
-        mtask.invoke
 
         define_post_maven_tasks
 
@@ -217,11 +299,23 @@ module RJack
         define_git_tag
       end
 
+      # For manifest, map destination jars from available jars in
+      # (jar_from) target/assembly. These are available since mvn
+      # package will be run first for the :manifest target.
+      def dest_jars
+        jars = FileList[ File.join( jar_from, "*.jar" ) ]
+        jars = jars.map { |j| File.basename( j ) }
+        jars.map { |j| File.join( jar_dest, j ) }
+      end
+
+      # Extract jar files from saved Manifest state, since
+      # neither from or dest jars may be available at call time.
       def jars
-        @jars = FileList[ "#{jars_from_path}/*.jar" ]
-        @jars.map! { |f| File.basename( f ) }
-        puts "TARPIT jars : #{@jars.inspect}"
-        #FIXME: Safe to do once?
+        unless @jars
+          files = read_file_list( 'Manifest.txt' ).select { |f| f =~ /\.jar$/ }
+          @jars = files.map { |f| File.basename( f ) }
+        end
+        puts "TARPIT: Manifest jars = #{@jars.inspect}"
         @jars
       end
 

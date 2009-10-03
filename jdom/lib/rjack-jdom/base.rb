ded9f864612332d24cf4da9ff8268d98d454c6bb
David Kellum <dek-oss@gravitext.com>
2009-10-03 12:32:23 -0700

    jdom: use same license; include Manifest.static; docs

--- jdom/lib/rjack-jdom/base.rb
+++ jdom/lib/rjack-jdom/base.rb
@@ -1,26 +1,17 @@
 #--
 # Copyright (C) 2009 David Kellum
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you
-# may not use this file except in compliance with the License.  You
-# may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.  See the License for the specific language governing
-# permissions and limitations under the License.
+# See README.rdoc for license terms.
 #++
 
 module RJack
   module JDOM
 
-    # Commons codec (java) version
+    # JDOM (java) version
+    # Note a jdom 1.1.1 preview is available (not yet in maven)
     JDOM_VERSION = '1.1'
 
-    # rjack gem version
+    # rjack gem version (reserve one more decimal for jdom itself)
     VERSION = JDOM_VERSION + '.0.0'
 
     JDOM_DIR = File.dirname( __FILE__ ) # :nodoc:

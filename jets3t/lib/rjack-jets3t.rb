2732eafb1bbea410c801a50faecd74ba945aad52
David Kellum <dek-oss@gravitext.com>
2009-10-03 09:59:34 -0700

    Initial tarpit based jets3t

---
+++ jets3t/lib/rjack-jets3t.rb
@@ -0,0 +1,29 @@
+#--
+# Copyright (C) 2009 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+require 'rubygems'
+
+require 'rjack-slf4j'
+require 'rjack-slf4j/jcl-over-slf4j'
+require 'rjack-commons-codec'
+require 'rjack-httpclient'
+require 'rjack-jets3t/base'
+
+module RJack
+  module JetS3t
+    require "#{JETS3T_DIR}/jets3t-#{JETS3T_VERSION}.jar"
+  end
+end

430a69fe20113685b0acd774686bcbbda2cc3cf4
David Kellum <dek-oss@gravitext.com>
2009-10-10 11:21:20 -0700

    Adjust copyrights.

--- slf4j/lib/rjack-slf4j/base.rb
+++ slf4j/lib/rjack-slf4j/base.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (C) 2008 David Kellum
+# Copyright (C) 2008-2009 David Kellum
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation files

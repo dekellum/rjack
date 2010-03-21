fa5893d4b1d5fe216a547eb1a200b61fc7837ffe
David Kellum <dek-oss@gravitext.com>
2010-03-20 19:23:52 -0700

    protobuf: Initial import.

---
+++ protobuf/lib/rjack-protobuf.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+
+require 'rjack-protobuf/base'
+
+module RJack
+  module ProtoBuf
+    require "#{PB_DIR}/protobuf-java-#{PB_VERSION}.jar"
+  end
+end
---
+++ protobuf/lib/rjack-protobuf/base.rb
@@ -0,0 +1,12 @@
+module RJack
+  module ProtoBuf
+
+    # Commons pool (java) version
+    PB_VERSION = '2.3.0'
+
+    # rjack gem version
+    VERSION = PB_VERSION + '.0'
+
+    PB_DIR = File.dirname( __FILE__ ) # :nodoc:
+  end
+end

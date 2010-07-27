688c3624ee2643d6bd8998e7e2680f2ab6505b97
David Kellum <dek-oss@gravitext.com>
2010-07-26 20:16:20 -0700

    neko: initial snapshot

---
+++ neko/lib/rjack-nekohtml/base.rb
@@ -0,0 +1,28 @@
+#--
+# Copyright (c) 2010 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+module RJack
+  module NekoHTML
+
+    # Upstream java version
+    NEKOHTML_VERSION = '1.9.14'
+
+    # rjack gem version
+    VERSION = NEKOHTML_VERSION + '.0'
+
+    NEKOHTML_DIR = File.dirname( __FILE__ ) # :nodoc:
+  end
+end

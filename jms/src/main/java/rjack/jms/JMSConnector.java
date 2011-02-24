774e19b151561341d9e8574b88a1b7b1ee84e615
David Kellum <dek-oss@gravitext.com>
2011-02-23 21:08:50 -0800

    jms: Import JMSConnector, SessionExecutor and friends

---
+++ jms/src/main/java/rjack/jms/JMSConnector.java
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2011 David Kellum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package rjack.jms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jms.Connection;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JMSConnector
+    implements ExceptionListener, Runnable
+{
+    public JMSConnector( JMSContext context )
+    {
+        _context = context;
+    }
+
+    public synchronized void addConnectListener( ConnectListener listener )
+    {
+        _listeners.add( listener );
+    }
+
+    public JMSContext context()
+    {
+        return _context;
+    }
+
+    public int minConnectPoll()
+    {
+        return _minConnectPoll;
+    }
+
+    public void setMinConnectPoll( int minConnectPoll )
+    {
+        _minConnectPoll = minConnectPoll;
+    }
+
+    public int maxConnectPoll()
+    {
+        return _maxConnectPoll;
+    }
+
+    public void setMaxConnectPoll( int maxConnectPoll )
+    {
+        _maxConnectPoll = maxConnectPoll;
+    }
+
+    public int maxConnectDelay()
+    {
+        return _maxConnectDelay;
+    }
+
+    public void setMaxConnectDelay( int maxConnectDelay )
+    {
+        _maxConnectDelay = maxConnectDelay;
+    }
+
+    public synchronized void start()
+    {
+        if( _running ) {
+            throw new IllegalStateException( "JMSConnector already running." );
+        }
+        _thread = new Thread( this, "jms-cntr" );
+        _thread.setDaemon( true );
+        _running = true; //Running as of now, avoid race in awaitConnection()
+        _thread.start();
+    }
+
+    public void stop() throws InterruptedException
+    {
+        Thread t = null;
+
+        synchronized ( this ) {
+            if( _running ) {
+                _running = false;
+                notifyAll();
+            }
+            t = _thread;
+        }
+
+        if( t != null ) t.join();
+    }
+
+    public void run()
+    {
+        try {
+            connectLoop();
+        }
+        catch( JMSException x ) {
+            _log.error( "Connection loop terminated with: ", x );
+        }
+        catch( NamingException x ) {
+            _log.error( "Connection loop terminated with: ", x );
+        }
+    }
+
+    public synchronized void connectLoop()
+        throws JMSException, NamingException
+    {
+        try {
+            _running = true;
+
+            while( _running ) {
+                connect();
+                wait( 1000 );
+            }
+        }
+        catch( InterruptedException i ) {
+            _log.warn( "In connectLoop:", i );
+        }
+        finally {
+            _running = false;
+            notifyAll();
+        }
+
+        //FIXME: Close connection on exit?
+    }
+
+    public synchronized Connection awaitConnection()
+        throws InterruptedException
+    {
+        while( _running && ( _connection == null ) ) {
+            wait( 1000 );
+        }
+
+        if( _connection == null ) {
+            throw new IllegalStateException( "JMSConnector not running." );
+        }
+
+        return _connection;
+    }
+
+    //FIXME: Support maximum awaitConnection() time?
+
+    public void onException( JMSException x )
+    {
+         if( _log.isDebugEnabled() ) _log.warn( "onException: ", x );
+         else _log.warn( "onException: {}", x.toString() );
+
+         Connection connection = null;
+         synchronized( this ) {
+             connection = _connection;
+             _connection = null;
+             notifyAll();
+         }
+         safeClose( connection );
+    }
+
+    /**
+     *  Called after createConnection, before connection.start,
+     *  to notify ConnectListener's.
+     */
+    private void onConnect( Connection connection )
+        throws JMSException, NamingException
+    {
+        for( ConnectListener listener : _listeners ) {
+            listener.onConnect( _context, connection );
+        }
+    }
+
+    private synchronized void connect()
+        throws JMSException, NamingException, InterruptedException
+    {
+        long sleep = _minConnectPoll;
+        long slept = 0;
+
+        while( _running && ( _connection == null ) ) {
+            Connection connection = null;
+            try {
+                connection = _context.createConnection();
+
+                // onConnect JMSException will be retried
+                // NamingExpetion will not
+                onConnect( connection );
+
+                connection.start();
+
+                _connection = connection;
+                connection = null;
+                _connection.setExceptionListener( this );
+                notifyAll();
+            }
+            catch( JMSException x ) {
+                if( slept < _maxConnectDelay ) {
+
+                    if( _log.isDebugEnabled() ) _log.warn( "On connect:", x );
+                    else _log.warn( "On connect: {}", x.toString() );
+
+                    long s = Math.min( sleep, _maxConnectDelay - slept );
+                    _log.info( "Waiting for {}ms before next connect attempt",
+                               s );
+                    wait( s );
+                    slept += s;
+                    sleep = Math.min( sleep * 2, _maxConnectPoll );
+                }
+                else throw x;
+            }
+            finally {
+                safeClose( connection ); // If an unset connection remains:
+                _context.close();
+            }
+        }
+    }
+
+    private void safeClose( Connection connection )
+    {
+        try {
+            if( connection != null ) connection.close();
+        }
+        catch( JMSException x ) {
+            if( _log.isDebugEnabled() ) {
+                _log.warn( "On connection close: ", x );
+            }
+            else _log.warn( "On connection close: {}", x.toString() );
+        }
+    }
+
+    private final JMSContext _context;
+    private final Logger _log = LoggerFactory.getLogger( getClass() );
+
+    private int _minConnectPoll  =    16; //ms
+    private int _maxConnectPoll  =  2048;
+    private int _maxConnectDelay = 30704;
+
+    private volatile boolean _running = false;
+
+    private Connection _connection = null;
+
+    private List<ConnectListener> _listeners = new ArrayList<ConnectListener>();
+
+    private Thread _thread = null;
+}
---
+++ jms/src/main/java/rjack/jms/SessionExecutor.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2011 David Kellum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package rjack.jms;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SessionExecutor<T extends SessionState>
+{
+    public SessionExecutor( JMSConnector connector,
+                            SessionStateFactory<T> factory )
+    {
+        _connector = connector;
+        _factory = factory;
+
+        BlockingOfferQueue queue = new BlockingOfferQueue( 1000 );
+
+        //FIXME: Config depth, threads, etc.
+
+        _execService =
+            new ThreadPoolExecutor( 1, 1,
+                                    1, TimeUnit.SECONDS,
+                                    queue,
+                                    new SessionThreadFactory() );
+    }
+
+    public void execute( SessionTask<T> task )
+        throws JMSException, NamingException, InterruptedException
+    {
+        try {
+            _execService.execute( task );
+
+            //FIXME: Or possibly just log these, as would be the case
+            //if added to queue?
+        }
+        catch( JMSRuntimeException x ) {
+            Throwable cause = x.getCause();
+
+            if( cause instanceof JMSException ) {
+                throw (JMSException) cause;
+            }
+            else if( cause instanceof NamingException ) {
+                throw (NamingException) cause;
+            }
+            else if( cause instanceof InterruptedException ) {
+                throw (InterruptedException) cause;
+            }
+        }
+    }
+
+    public void shutdown()
+    {
+        _execService.shutdown();
+    }
+
+    public boolean awaitTermination( long timeout, TimeUnit unit )
+        throws InterruptedException
+    {
+        return _execService.awaitTermination( timeout, unit );
+    }
+
+    static class SessionThread<T extends SessionState> extends Thread
+    {
+        public SessionThread( Runnable r,
+                              JMSConnector connector,
+                              SessionStateFactory<T> factory,
+                              int factoryId,
+                              int threadId )
+            throws InterruptedException, JMSException, NamingException
+        {
+            super( r, String.format( "jms-st-%d-%d", factoryId, threadId ) );
+            Connection connection = connector.awaitConnection();
+            _state = factory.createSessionState( connector.context(),
+                                                 connection );
+        }
+
+        public void run()
+        {
+            try {
+                super.run();
+            }
+            catch( JMSRuntimeException x ) {
+                _log.warn( "Exit due to: ", x.getCause() );
+            }
+            finally {
+                close();
+            }
+        }
+
+        public T state()
+        {
+            return _state;
+        }
+
+        private void close()
+        {
+            if( _state != null ) {
+                try {
+                    _state.close();
+                }
+                catch( JMSException x ) {
+                    _log.warn( "On close: ", x );
+                }
+            }
+        }
+
+        private final T _state;
+        private final Logger _log = LoggerFactory.getLogger( getClass() );
+
+    }
+
+    static final class JMSRuntimeException
+        extends RuntimeException
+    {
+        public JMSRuntimeException( Exception cause )
+        {
+            super( cause );
+        }
+    }
+
+    private final class SessionThreadFactory implements ThreadFactory
+    {
+        public Thread newThread( Runnable r )
+        {
+            try {
+                return new SessionThread<T>( r,
+                                             _connector,
+                                             _factory,
+                                             _factoryId,
+                                             _threadCounter.incrementAndGet() );
+            }
+            catch( InterruptedException x ) {
+                Thread.currentThread().interrupt();
+                throw new JMSRuntimeException( x );
+            }
+            catch( JMSException x ) {
+                throw new JMSRuntimeException( x );
+            }
+            catch( NamingException x ) {
+                throw new JMSRuntimeException( x );
+            }
+        }
+
+        private final int _factoryId = _factoryCounter.incrementAndGet();
+        private final AtomicInteger _threadCounter = new AtomicInteger( 0 );
+    }
+
+    private static final class BlockingOfferQueue
+        extends LinkedBlockingQueue<Runnable>
+    {
+        public BlockingOfferQueue( int capacity )
+        {
+            super( capacity );
+        }
+
+        @Override
+        public boolean offer( Runnable e )
+        {
+            try {
+                put( e );
+                return true;
+            }
+            catch( InterruptedException x ) {
+                Thread.currentThread().interrupt();
+                return false;
+            }
+        }
+    }
+    private final ExecutorService _execService;
+    private final JMSConnector _connector;
+    private final SessionStateFactory<T> _factory;
+
+    private static AtomicInteger _factoryCounter = new AtomicInteger( 0 );
+}

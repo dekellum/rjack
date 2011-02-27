35e655291bde20b14d641a4f0651f722c35f586d
David Kellum <dek-oss@gravitext.com>
2011-02-27 09:35:25 -0800

    jms: Split out mocks for reuse

--- jms/test/setup.rb
+++ jms/test/setup.rb
@@ -20,6 +20,8 @@ test_dir = File.dirname( __FILE__ )
 ldir = File.join( test_dir, "..", "lib" )
 $LOAD_PATH.unshift( ldir ) unless $LOAD_PATH.include?( ldir )
 
+$LOAD_PATH.unshift( test_dir ) unless $LOAD_PATH.include?( test_dir )
+
 require 'rubygems'
 require 'rjack-logback'
 
--- jms/test/test_jms_connector.rb
+++ jms/test/test_jms_connector.rb
@@ -18,97 +18,67 @@
 
 require File.join( File.dirname( __FILE__ ), "setup" )
 
-module RJack::JMS
+require 'mocks'
 
-  class Tracked
-    attr_reader :called
+class TestJMSConnector < MiniTest::Unit::TestCase
+  include RJack::JMS
 
-    def initialize
-      @called = []
-    end
+  import 'javax.jms.JMSException'
+  import 'javax.naming.NamingException'
 
-    def method_missing( name, *args )
-      called << name
-    end
+  def setup
+    @connector = JMSConnector.new( @context = Context.new )
   end
 
-  class Connection < Tracked
-    include javax.jms.Connection
-
-    attr_accessor :exception_listener
+  def test_start_stop
+    @connector.start
+    # May or may not actually run in time before stop.
+    # But shouldn't deadlock.
+    @connector.stop
   end
 
-  class Context < Tracked
-    include JMSContext
+  def test_start_await_stop
+    @connector.add_connect_listener( listener = Listener.new )
+    @connector.start
 
-    def create_connection
-      called << :create_connection
-      Connection.new
-    end
+    con = @connector.await_connection
+    assert_kind_of( Connection, con )
+    assert_includes( listener.called, :onConnect )
+    assert_includes( con.called, :start )
+
+    @connector.stop
+    assert_includes( @context.called, :close )
   end
 
-  class Listener < Tracked
-    include ConnectListener
+  def test_on_exception
+    @connector.start
+
+    con = @connector.await_connection
+    assert_includes( con.called, :start )
+
+    con.exception_listener.on_exception( JMSException.new( "test" ) )
+    assert_includes( con.called, :close )
+
+    con2 = @connector.await_connection
+    refute_equal( con, con2 )
+    assert_includes( con2.called, :start )
+
+    @connector.stop
+    assert_includes( @context.called, :close )
   end
 
-  class TestJMSConnector < MiniTest::Unit::TestCase
-    import 'javax.jms.JMSException'
-    import 'javax.naming.NamingException'
+  def test_failed_listener
+    @connector.add_connect_listener( listener = Listener.new )
 
-    def setup
-      @connector = JMSConnector.new( @context = Context.new )
+    def listener.on_connect( *args )
+      raise NamingException.new( "test" )
     end
 
-    def test_start_stop
-      @connector.start
-      # May or may not actually run in time before stop.
-      # But shouldn't dead lock.
-      @connector.stop
+    assert_raises( NativeException, NamingException ) do
+      @connector.connect_loop
     end
 
-    def test_start_await_stop
-      @connector.add_connect_listener( listener = Listener.new )
-      @connector.start
-
-      con = @connector.await_connection
-      assert_kind_of( Connection, con )
-      assert_includes( listener.called, :onConnect )
-      assert_includes( con.called, :start )
-
-      @connector.stop
-      assert_includes( @context.called, :close )
-    end
-
-    def test_on_exception
-      @connector.start
-
-      con = @connector.await_connection
-      assert_includes( con.called, :start )
-
-      con.exception_listener.on_exception( JMSException.new( "test" ) )
-      assert_includes( con.called, :close )
-
-      con2 = @connector.await_connection
-      refute_equal( con, con2 )
-      assert_includes( con2.called, :start )
-
-      @connector.stop
-      assert_includes( @context.called, :close )
-    end
-
-    def test_failed_listener
-      @connector.add_connect_listener( listener = Listener.new )
-
-      def listener.on_connect( *args )
-        raise NamingException.new( "test" )
-      end
-
-      assert_raises( NativeException, NamingException ) do
-        @connector.connect_loop
-      end
-
-      assert_includes( @context.called, :close )
-    end
-
+    assert_includes( @context.called, :close )
   end
+
 end

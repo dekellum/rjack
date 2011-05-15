393903b7b614c8f481f97f40233ca3e119a6f8dc
David Kellum <dek-oss@gravitext.com>
2011-05-14 19:29:05 -0700

    logback-redirect: Initial import of 'logback' gem compatibility wrapper

---
+++ logback-redirect/lib/logback/base.rb
@@ -0,0 +1,17 @@
+#--
+# Copyright (c) 2008-2011 David Kellum
+#
+# logback is free software: you can redistribute it and/or
+# modify it under the terms of either of following licenses:
+#
+#   GNU Lesser General Public License v3 or later
+#   Eclipse Public License v1.0
+#
+# rjack-logback is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#++
+
+module LogbackRedirect
+  VERSION = '1.1'
+end

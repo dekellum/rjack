02030049cb56a54e0bfc4232e9b1a2439dbcaf56
David Kellum <dek-oss@gravitext.com>
2011-05-15 12:36:18 -0700

    jetty: Increase pool min_threads to 2 (fixes test hang with upgrade > 7.2)

--- jetty/lib/rjack-jetty.rb
+++ jetty/lib/rjack-jetty.rb
@@ -160,7 +160,7 @@ module RJack
       # (default 20).
       def create_pool
         pool = QueuedThreadPool.new
-        pool.min_threads = [ @min_threads || ( @max_threads / 4 ), 1 ].max
+        pool.min_threads = [ @min_threads || ( @max_threads / 4 ), 2 ].max
         pool.max_threads = [ @max_threads, 3 ].max
         pool
       end

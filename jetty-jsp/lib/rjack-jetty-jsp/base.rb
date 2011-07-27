c942ce8e6d3fb8dc3cfa9aa9852a1cc28bebb778
David Kellum <dek-oss@gravitext.com>
2011-07-26 20:40:55 -0700

    jetty-jsp: Upgrade to 7.4.5.v20110725

--- jetty-jsp/lib/rjack-jetty-jsp/base.rb
+++ jetty-jsp/lib/rjack-jetty-jsp/base.rb
@@ -17,10 +17,10 @@
 module RJack
   module Jetty
     module Jsp
-      JETTY_VERSION = '7.4.4'
+      JETTY_VERSION = '7.4.5'
       GEM_VERSION = '0'
       VERSION = [ JETTY_VERSION, GEM_VERSION ].join( '.' )
-      JETTY_BUILD = 'v20110707'
+      JETTY_BUILD = 'v20110725'
       JSP_VERSION = '2.1'
 
       JETTY_JSP_DIR = File.dirname( __FILE__ ) # :nodoc:

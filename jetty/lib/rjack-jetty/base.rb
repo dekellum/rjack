73c19593269465a8b080f85f6ba89456d79fa895
David Kellum <dek-oss@gravitext.com>
2011-09-01 18:17:40 -0700

    jetty: Preliminary upgrade to 7.5.0.RC2

--- jetty/lib/rjack-jetty/base.rb
+++ jetty/lib/rjack-jetty/base.rb
@@ -18,7 +18,7 @@ module RJack
   module Jetty
     JETTY_VERSION = '7.5.0'
     VERSION = JETTY_VERSION + '.0'
-    JETTY_BUILD = 'RC1'
+    JETTY_BUILD = 'RC2'
     SERVLET_API_VERSION = '2.5'
 
     JETTY_DIR = File.dirname( __FILE__ ) # :nodoc:

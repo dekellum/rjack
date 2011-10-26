728b0224edff7933648fc25033f33eac8854d8e2
David Kellum <dek-oss@gravitext.com>
2011-10-25 19:22:10 -0700

    jetty: Upgrade to 8.0.4.v20111024

--- jetty/lib/rjack-jetty/base.rb
+++ jetty/lib/rjack-jetty/base.rb
@@ -16,9 +16,9 @@
 
 module RJack
   module Jetty
-    JETTY_VERSION = '8.0.3'
+    JETTY_VERSION = '8.0.4'
     VERSION = JETTY_VERSION + '.0'
-    JETTY_BUILD = 'v20111011'
+    JETTY_BUILD = 'v20111024'
     SERVLET_API_VERSION = '3.0'
     SERVLET_API_BUILD = '20100224'
 

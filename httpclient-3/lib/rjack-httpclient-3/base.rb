a649a71bda32bae346ffea56b9ac3c2518ee870b
David Kellum <dek-oss@gravitext.com>
2012-06-08 16:21:30 -0700

    httpclient-3: Bump to 3.1.6

--- httpclient-3/lib/rjack-httpclient-3/base.rb
+++ httpclient-3/lib/rjack-httpclient-3/base.rb
@@ -17,7 +17,7 @@
 module RJack
   module HTTPClient3
     HTTPCLIENT_VERSION = '3.1'
-    VERSION = HTTPCLIENT_VERSION + '.5'
+    VERSION = HTTPCLIENT_VERSION + '.6'
 
     HTTPCLIENT_DIR = File.dirname( __FILE__ ) # :nodoc:
   end

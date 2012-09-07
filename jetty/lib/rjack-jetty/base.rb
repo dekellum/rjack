650cfd02da15e3bc7d3d2f395c396ed5cd36646e
David Kellum <dek-oss@gravitext.com>
2012-09-06 21:29:37 -0700

    jetty: Update to 7.6.6.v20120903

--- jetty/lib/rjack-jetty/base.rb
+++ jetty/lib/rjack-jetty/base.rb
@@ -16,9 +16,9 @@
 
 module RJack
   module Jetty
-    JETTY_VERSION = '7.6.5'
+    JETTY_VERSION = '7.6.6'
     VERSION = JETTY_VERSION + '.0'
-    JETTY_BUILD = 'v20120716'
+    JETTY_BUILD = 'v20120903'
     SERVLET_API_VERSION = '2.5'
 
     JETTY_DIR = File.dirname( __FILE__ ) # :nodoc:

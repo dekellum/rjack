2e88413bcc57a0b972a84a42cdea17b0f4919007
David Kellum <dek-oss@gravitext.com>
2012-11-08 15:41:53 -0800

    httpclient-4: Upgrade to httpclient/core 4.2.2

--- httpclient-4/lib/rjack-httpclient-4/base.rb
+++ httpclient-4/lib/rjack-httpclient-4/base.rb
@@ -16,7 +16,7 @@
 
 module RJack
   module HTTPClient4
-    HTTPCLIENT_VERSION = '4.2.1'
+    HTTPCLIENT_VERSION = '4.2.2'
     VERSION = HTTPCLIENT_VERSION + '.0'
 
     HTTPCLIENT_DIR = File.dirname( __FILE__ ) # :nodoc:

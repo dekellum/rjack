7f0b51f2a4b3cf867cc622ae82a90da35ff62260
David Kellum <dek-oss@gravitext.com>
2013-11-21 10:17:30 -0800

    jetty: Upgrade to  9.0.7.v20131107

--- jetty/lib/rjack-jetty/base.rb
+++ jetty/lib/rjack-jetty/base.rb
@@ -16,9 +16,9 @@
 
 module RJack
   module Jetty
-    JETTY_VERSION = '9.0.6'
+    JETTY_VERSION = '9.0.7'
     VERSION = JETTY_VERSION + '.0'
-    JETTY_BUILD = 'v20130930'
+    JETTY_BUILD = 'v20131107'
     SERVLET_API_VERSION = '3.0.0'
     SERVLET_API_BUILD = 'v201112011016'
 

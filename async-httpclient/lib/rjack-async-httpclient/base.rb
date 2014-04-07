9a74323abd2ec5dc7fd4a3fb8f186f63fe4807ff
David Kellum <dek-oss@gravitext.com>
2014-04-07 09:04:22 -0700

    async-httpclient: Upgrade to 1.8.5 (netty bump still needed)

--- async-httpclient/lib/rjack-async-httpclient/base.rb
+++ async-httpclient/lib/rjack-async-httpclient/base.rb
@@ -16,7 +16,7 @@
 
 module RJack
   module AsyncHTTPClient
-    CLIENT_VERSION = '1.7.24'
+    CLIENT_VERSION = '1.8.5'
     VERSION = CLIENT_VERSION + '.0'
 
     CLIENT_DIR = File.dirname( __FILE__ ) # :nodoc:

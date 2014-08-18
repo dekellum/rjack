d415be8f2e418e397e2b98238b19c55e6f97698e
David Kellum <dek-oss@gravitext.com>
2014-08-18 15:04:45 -0700

    nekohtml: upgrade to 1.9.21

--- nekohtml/lib/rjack-nekohtml/base.rb
+++ nekohtml/lib/rjack-nekohtml/base.rb
@@ -18,7 +18,7 @@ module RJack
   module NekoHTML
 
     # Upstream java version
-    NEKOHTML_VERSION = '1.9.20'
+    NEKOHTML_VERSION = '1.9.21'
 
     # rjack gem version
     VERSION = NEKOHTML_VERSION + '.0'

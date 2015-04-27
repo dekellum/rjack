27a5f055cde0f164e8e69cd2877e3944321faaf3
David Kellum <dek-oss@gravitext.com>
2015-04-27 10:07:18 -0700

    jetty: Bump to version 9.2.10.1

--- jetty/lib/rjack-jetty/base.rb
+++ jetty/lib/rjack-jetty/base.rb
@@ -17,7 +17,7 @@
 module RJack
   module Jetty
     JETTY_VERSION = '9.2.10'
-    VERSION = JETTY_VERSION + '.0'
+    VERSION = JETTY_VERSION + '.1'
     JETTY_BUILD = 'v20150310'
     SERVLET_API_VERSION = '3.1.0'
 

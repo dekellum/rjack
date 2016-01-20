78eaff97ff810cfaf6c97dfa17b559ce070d875f
David Kellum <dek-oss@gravitext.com>
2016-01-20 15:43:32 -0800

    jetty: Fix SSL test with ciphers set as per Mozilla's intermediate suite
    Jetty 9.3.7 changes the default ciphers for SSL connectors.
    
    Manual testing with Firefox, MRI, and curl all show this is working fine for these clients.
    
    For JRuby, see jruby/jruby#1738

--- jetty/test/test_jetty.rb
+++ jetty/test/test_jetty.rb
@@ -34,6 +34,7 @@ require 'rjack-jetty/rewrite'
 require 'rjack-jetty/test-servlets'
 
 require 'net/http'
+require 'openssl'
 
 class TestJetty < MiniTest::Unit::TestCase
   include RJack::Jetty
@@ -208,7 +209,10 @@ class TestJetty < MiniTest::Unit::TestCase
   def get( path, port, scheme = :tcp )
     args = [ 'localhost', port, nil, nil, nil, nil ]
     if scheme == :ssl
-      args << { use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE }
+      args << { use_ssl: true,
+                # From https://wiki.mozilla.org/Security/Server_Side_TLS (intermediate)
+                ciphers: "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA",
+                verify_mode: OpenSSL::SSL::VERIFY_NONE }
     end
     Net::HTTP.start( *args ) do |http|
       http.open_timeout = 1.0

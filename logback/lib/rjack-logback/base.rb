006c86605dda675d3f24471df38fe9d55345e127
David Kellum <dek-oss@gravitext.com>
2017-06-19 12:54:22 -0700

    logback: Update 1.9.0 (jar 1.1.11)

--- logback/lib/rjack-logback/base.rb
+++ logback/lib/rjack-logback/base.rb
@@ -15,10 +15,10 @@
 module RJack
   module Logback
     # Logback java version
-    LOGBACK_VERSION = '1.1.7'
+    LOGBACK_VERSION = '1.1.11'
 
     # Logback gem version
-    VERSION = '1.8.1'
+    VERSION = '1.9.0'
 
     LOGBACK_DIR = File.dirname(__FILE__) # :nodoc:
   end

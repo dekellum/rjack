bb3ee17904c80ddbb414239a2d595584d1a06681
David Kellum <dek-oss@gravitext.com>
2019-01-10 13:34:22 -0800

    jetty: Upgrade to 9.4.14.v20181114 (release)

--- jetty/lib/rjack-jetty/base.rb
+++ jetty/lib/rjack-jetty/base.rb
@@ -16,9 +16,9 @@
 
 module RJack
   module Jetty
-    JETTY_VERSION = '9.4.6'
+    JETTY_VERSION = '9.4.14'
     VERSION = JETTY_VERSION + '.0'
-    JETTY_BUILD = 'v20170531'
+    JETTY_BUILD = 'v20181114'
     SERVLET_API_VERSION = '3.1.0'
 
     JETTY_DIR = File.dirname( __FILE__ ) # :nodoc:
